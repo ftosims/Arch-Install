97a914453e9ba7a6532f41217845896fd7e9c822
Blind_one <ftosims@outlook.com>
2017-10-20 18:40:04 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 #联网
-wifi-menu
+#wifi-menu
 #分区
 read -p "sda1 efi sda2 / sda3 /home sda4 swap"
 parted /dev/sda
 #格式化分区
+sleep 2s
 mkfs.vfat -F32 /dev/sda1
 mkfs.ext4 /dev/sda2
 mkfs.ext4 /dev/sda3
 mkswap    /dev/sda4
 swapon    /dev/sda4
 #挂载分区
+sleep 2s
 mount /dev/sda2 /mnt
 mkdir /mnt/home
-mkdir /mnt/boot/efi
+mkdir -p /mnt/boot/efi
+sleep 2s
 mount /dev/sda3 /mnt/home
 mount /dev/sda1 /mnt/boot/efi
 #修改镜像列表
